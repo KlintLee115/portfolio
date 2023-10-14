73a8a494dc053d4d1aeec13be3b92c42735e75ab
KlintLee115 <54990359+KlintLee115@users.noreply.github.com>
2023-10-14 12:38:31 -0600

    fixed build bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import './App.css';
