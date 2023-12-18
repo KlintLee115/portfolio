0d16d8ef900a82d8973b675dc14d679f11e5f402
KlintLee115 <54990359+KlintLee115@users.noreply.github.com>
2023-12-17 21:21:31 -0700

    added pg

--- src/ProjectsFragment/Projects.ts
+++ src/ProjectsFragment/Projects.ts
@@ -1,6 +1,6 @@
 const ProjectsList = {
     'Reversed_Tetris': { displayedName: 'Reversed Tetris', techUsed:['Html', 'Vanilla JS', 'TypeScript', 'CSS'], websiteLink: 'https://fluffy-choux-51e2bc.netlify.app/', githubLink: 'https://github.com/KlintLee115/ReversedTetris', description: "Created using HTML, CSS, and JS to strengthen my foundation of front end. Inspired by the popular Tetris game, this is a reversed version, where pieces move from bottom to top." },
-    'KSports':{displayedName:"K Sports", techUsed:['NextJS','Tailwind CSS', 'Stripe','ExpressJS'], websiteLink: 'https://ksports-klint-lees-projects.vercel.app/', githubLink:'https://github.com/KlintLee115/ksports', description:"A simple full stack e commerce website"},
+    'KSports':{displayedName:"K Sports", techUsed:['NextJS', 'PostgresDB','Tailwind CSS', 'Stripe','ExpressJS'], websiteLink: 'https://ksports-klint-lees-projects.vercel.app/', githubLink:'https://github.com/KlintLee115/ksports', description:"A simple full stack e commerce website"},
     'Sepakat_lk': { displayedName: 'Sepakat Lk', techUsed:['React'], websiteLink: 'https://chipper-narwhal-3bc4e8.netlify.app/', githubLink:'https://github.com/KlintLee115/sepakatLK', description: "The landing page of a website for a Malaysia construction company called Sepakat LK."},
 }
 
