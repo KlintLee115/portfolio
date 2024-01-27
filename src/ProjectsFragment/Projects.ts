1d79c48b6d8e4cef802bd007a9916d738377d9f3
KlintLee115 <programmerder@gmail.com>
2024-01-26 23:56:53 -0700

    Update Projects.ts

--- src/ProjectsFragment/Projects.ts
+++ src/ProjectsFragment/Projects.ts
@@ -1,7 +1,7 @@
 const ProjectsList = {
-    'Reversed_Tetris': { displayedName: 'Reversed Tetris', techUsed:['Html', 'Vanilla JS', 'TypeScript', 'CSS'], websiteLink: 'https://fluffy-choux-51e2bc.netlify.app/', githubLink: 'https://github.com/KlintLee115/ReversedTetris', description: "Created using HTML, CSS, and JS to strengthen my foundation of front end. Inspired by the popular Tetris game, this is a reversed version, where pieces move from bottom to top." },
-    'KSports':{displayedName:"K Sports", techUsed:['NextJS', 'AWS S3', 'PostgresDB','Tailwind CSS', 'Stripe','ExpressJS'], websiteLink: 'https://ksports-three.vercel.app/', githubLink:'https://github.com/KlintLee115/ksports', description:"A simple full stack e commerce website"},
-    'Sepakat_lk': { displayedName: 'Sepakat Lk', techUsed:['React'], websiteLink: 'https://chipper-narwhal-3bc4e8.netlify.app/', githubLink:'https://github.com/KlintLee115/sepakatLK', description: "The landing page of a website for a Malaysia construction company called Sepakat LK."},
+    'Reversed_Tetris': { displayedName: 'Reversed Tetris', techUsed:['Html', 'Vanilla JS', 'TypeScript', 'CSS'], websiteLink: 'https://reversed-tetris.netlify.app/', githubLink: 'https://github.com/KlintLee115/ReversedTetris', description: "Created using HTML, CSS, and JS to strengthen my foundation of front end. Inspired by the popular Tetris game, this is a reversed version, where pieces move from bottom to top." },
+    'KSports':{displayedName:"K Sports", techUsed:['NextJS', 'AWS S3', 'PostgresDB','Tailwind CSS', 'Stripe','ExpressJS'], websiteLink: 'https://k-sports.vercel.app/', githubLink:'https://github.com/KlintLee115/ksports', description:"A simple full stack e commerce website"},
+    'Sepakat_lk': { displayedName: 'Sepakat Lk', techUsed:['React'], websiteLink: 'https://sepakatlk.netlify.app/', githubLink:'https://github.com/KlintLee115/sepakatLK', description: "The landing page of a website for a Malaysia construction company called Sepakat LK."},
 }
 
 export { ProjectsList }
