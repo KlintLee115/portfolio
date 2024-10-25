66aaac140184ff8efe4b40b671e28d841f99f0fd
KlintLee115 <klintlee1@gmail.com>
2024-10-25 17:10:52 -0600

    update article

--- src/Data/Articles.ts
+++ src/Data/Articles.ts
@@ -12,8 +12,8 @@ const ArticlesList: { [key: string]: ContentType } = {
         image: getImgPath('Reversed_Tetris')
     },
     'Database_Fts': {
-        displayedName: 'Reversed Tetris',
-        description: "This article is about how I planned my Reversed Tetris game, including the multiplayer mode and game mechanism.",
+        displayedName: 'Improving text search in relational databases',
+        description: "This article goes through the basics of database indexes, and why common text searching methods like %{searchterm}% may be suboptimal. \n And finally, it talks about the solution to these problems with a benchmark result.",
         url: "https://www.linkedin.com/feed/update/urn:li:activity:7249086073828487169/",
         image: getImgPath('Database_Fts')
     },
